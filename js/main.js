3227e34086308975cdf7caef77212df40fc33f8e
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-12-03 20:08:14 -0300

    Criando, alterando e deletando propriedades

--- js/main.js
+++ js/main.js
@@ -1,73 +1,98 @@
-// var meuNumero = 20;
-// var meuTexto = 'Exemplo';
+// // var meuNumero = 20;
+// // var meuTexto = 'Exemplo';
 
-// console.log(meuNumero + meuTexto);
-
-
-/*escopo global
-só pode ser usado o var
+// // console.log(meuNumero + meuTexto);
 
 
-function(){
-    escopo de função
-}
-pode ser usado o var, let e o const
-
-{
-    escopo de bloco
-}
-pode ser usado o let e o const
-*/
-
-//==============STRINGS===================
-
-// //Retorna o tamanho de uma string
-// const textSize = 'Texto'.length;
-// console.log(`Quantidade de letras: ${textSize}`)
-
-//Retorna um array quebrando a string por um delimitador
-// const splittedText = 'Texto'.split('x')
-// console.log('\nArray com as posições separadas pelo delimitador:', splittedText)
-
-//Busca por um valor e substitui por outro
-// const replacedText = 'Ação'.replace('A' , 'Motiva')
-// console.log('\nSubstituiçãao de valor:' , replacedText)
-
-//Retorna a "fatia" de um valor
-// const lastChar = 'Texto'.slice(-1)
-// console.log('\nÚltima letra de uma string:' , lastChar)
+// /*TODOescopo global
+// só pode ser usado o var
 
 
-// const allWhitoutLastChar = 'Texto'.slice(0,-1)
-// console.log('\nValor da string da primeira letra menos a última:' , allWhitoutLastChar)
+// function(){
+//     TODOescopo de função
+// }
+// pode ser usado o var, let e o const
 
-// const firstLetter = 'Texto'.slice(1)
-// console.log('\nValor da primeira letra da string: ', firstLetter)
+// {
+//     TODOescopo de bloco
+// }
+// pode ser usado o let e o const
+// */
 
-// const secondToEnd = 'Texto'.slice(0,1)
-// console.log('\nValor da string da segunda letra até a última: ', secondToEnd)
+// //==============STRINGS===================
 
-// //Retorna N caracteres a partir de uma posição
-// const twoCharsBeforeFirstPos = 'Texto'.substr(0,1)
-// console.log('\nAs duas primeiras letras são: ', twoCharsBeforeFirstPos)
+// // //TODORetorna o tamanho de uma string
+// // const textSize = 'Texto'.length;
+// // console.log(`Quantidade de letras: ${textSize}`)
+
+// //TODORetorna um array quebrando a string por um delimitador
+// // const splittedText = 'Texto'.split('x')
+// // console.log('\nArray com as posições separadas pelo delimitador:', splittedText)
+
+// //TODOBusca por um valor e substitui por outro
+// // const replacedText = 'Ação'.replace('A' , 'Motiva')
+// // console.log('\nSubstituiçãao de valor:' , replacedText)
+
+// //TODO Retorna a "fatia" de um valor 
+// // const lastChar = 'Texto'.slice(-1)
+// // console.log('\nÚltima letra de uma string:' , lastChar)
 
 
-//===============NUMBER========================
+// // const allWhitoutLastChar = 'Texto'.slice(0,-1)
+// // console.log('\nValor da string da primeira letra menos a última:' , allWhitoutLastChar)
 
-// const myNumber = 12.4032
+// // const firstLetter = 'Texto'.slice(1)
+// // console.log('\nValor da primeira letra da string: ', firstLetter)
 
-// //Transformar número para string
-// const numberAsString = myNumber.toString()
-// console.log('Número transformado em string: ', numberAsString)
+// // const secondToEnd = 'Texto'.slice(0,1)
+// // console.log('\nValor da string da segunda letra até a última: ', secondToEnd)
 
-// //Retorna número com um número de casas decimais
-// const fixedTwoDecimals = myNumber.toFixed(2)
-// console.log('\nNúmero com duas casas decimais: ',fixedTwoDecimals)
+// // //TODORetorna N caracteres a partir de uma posição
+// // const twoCharsBeforeFirstPos = 'Texto'.substr(0,1)
+// // console.log('\nAs duas primeiras letras são: ', twoCharsBeforeFirstPos)
 
-// //Transforma uma string em float
-// console.log('\nString parseada para float: ', parseFloat('13.22'))
 
-// //Transforma uma string em int
-// console.log('\nString parseada para int: ', parseInt('13.23'))
+// //===============NUMBER========================
 
-//=================OBJECT========================
+// // const myNumber = 12.4032
+
+// // //TODOTransformar número para string
+// // const numberAsString = myNumber.toString()
+// // console.log('Número transformado em string: ', numberAsString)
+
+// // //TODORetorna número com um número de casas decimais
+// // const fixedTwoDecimals = myNumber.toFixed(2)
+// // console.log('\nNúmero com duas casas decimais: ',fixedTwoDecimals)
+
+// // //TODOTransforma uma string em float
+// // console.log('\nString parseada para float: ', parseFloat('13.22'))
+
+// // //TODOTransforma uma string em int
+// // console.log('\nString parseada para int: ', parseInt('13.23'))
+
+// //=================OBJECT========================
+// let user ={
+//     name:'Luan'
+// };
+
+// //TODOAlterando a propriedade do objeto
+// //console.log(user);
+
+// user.name = 'Outro nome';
+// //console.log(user);
+
+// user['name'] = 'Outro nome 2';
+// //console.log(user);
+
+// const prop = 'name';
+// user[prop] = 'Outro nome 3';
+
+// //console.log(user);
+
+// //TODOCriando uma propriedade
+// user.lastName = 'da Silva Ramalho';
+
+
+// //TODODeletando uma propriedade
+// delete user.name;
+// console.log(user);
