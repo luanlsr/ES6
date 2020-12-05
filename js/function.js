ddc82c46345638b6e90efedafba7a8469d6f733f
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-12-05 06:52:46 -0300

    Criado arquivos nominais

--- js/function.js
+++ js/function.js
@@ -1,15 +1,45 @@
 function fn(){
     return 'Code here';
 }
-
+// TODOApenas uma função, não precisa por o return.
 const arrowFn = () => 'Code here';
 const arrowFn2 = () =>{
     //Mais de uma expressão
     return 'Code here'
 }
 
-//Funções também são objetos
+//TODOFunções também são objetos
 fn.prop = 'Posso criar propriedades';
 
-console.log(fn);
+console.log(fn());
 console.log(fn.prop);
+
+//TODOReceber parãmetros
+const logValue = value => console.log(value);
+const logFnResult = fnParam => console.log(fnParam()); 
+
+logFnResult(fn);
+
+//TODOReceber e retornar funções
+const controlFnExec = fnParam => allowed =>{
+    if (allowed){
+        fnParam();
+    }
+}
+
+const handleFnExecution = controlFnExec(fn);
+
+handleFnExecution(true);// Executará a função fn
+handleFnExecution();//Não executará a função fn
+
+//TODOControlfn como função
+function controlFnExec(fnParam){
+    return function(allowed){
+        if (allowed){
+        fnParam();
+        }
+    }
+}
+
+
+
